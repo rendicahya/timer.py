29396a60008905a5308bf90ee2cf6c13bc03d48d
rendicahya <rendicahya@gmail.com>
2021-02-08 15:52:06 +0700

    Added method: get_elapsed (and continue)

--- timerpy/timer.py
+++ timerpy/timer.py
@@ -1,11 +1,11 @@
-from math import floor
 from time import perf_counter
+from time import sleep
 
 from print_color import print as print_color
 
 
 class Timer:
-    def __init__(self, label=None, format='%d:%d:%02d.%s', ms_digits=3, color='green'):
+    def __init__(self, label='timer.py', format='%02d:%02d:%02d.%s', ms_digits=3, color='green'):
         self.label = label
         self.start_time = None
         self.format = format
@@ -15,12 +15,30 @@ class Timer:
     def start(self):
         self.start_time = perf_counter()
 
+    def get_elapsed(self):
+        return perf_counter() - self.start_time
+
     def stop(self):
-        time = perf_counter() - self.start_time
+        if self.start_time is None:
+            print_color('Timer not started', tag=self.label, tag_color='red', color='white')
+        else:
+            stop_time = perf_counter()
+            elapsed = stop_time - self.start_time
+            self.start_time = None
+
+            print_color(self.format_time(elapsed), tag=self.label, tag_color=self.color, color='white')
+
+    def format_time(self, time):
         hour = time // 3600
         min = time % 3600 // 60
-        sec = floor(time % 60)
+        sec = time % 60
         ms = str(time % 1)[2:self.ms_digits + 2]
-        time_str = self.format % (hour, min, sec, ms)
 
-        print_color(time_str, tag=self.label, tag_color=self.color, color='white')
+        return self.format % (hour, min, sec, ms)
+
+
+if __name__ == '__main__':
+    t = Timer('test')
+    # t.start()
+    sleep(1)
+    t.stop()
