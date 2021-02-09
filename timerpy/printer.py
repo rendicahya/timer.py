a7071846d150fd3c3d1336f6b3db06f55b5e9877
rendicahya <rendicahya@gmail.com>
2021-02-09 22:46:17 +0700

    Adding type hinting.

--- timerpy/printer.py
+++ timerpy/printer.py
@@ -2,11 +2,11 @@ from print_color import print as print_color
 
 
 class Printer:
-    def __init__(self, tag):
+    def __init__(self, tag: str):
         self.tag = tag
 
-    def error(self, text):
+    def error(self, text: str) -> None:
         print_color(text, tag=self.tag, tag_color='red', color='white')
 
-    def info(self, text):
+    def info(self, text: str) -> None:
         print_color(text, tag=self.tag, tag_color='green', color='white')
--- timerpy/timer.py
+++ timerpy/timer.py
@@ -5,7 +5,8 @@ from timerpy.utils import format_time
 
 
 class Timer:
-    def __init__(self, tag='timer.py', format='%02d:%02d:%02d.%s', ms_digits=3, color='green'):
+    def __init__(self, tag: str = 'timer.py', format: str = '%02d:%02d:%02d.%s', ms_digits: int = 3,
+                 color: str = 'green'):
         self.time_data = []
         self.is_started = False
         self.start_time = None
@@ -14,14 +15,14 @@ class Timer:
         self.color = color
         self.print = Printer(tag)
 
-    def start(self):
+    def start(self) -> None:
         if self.is_started:
             self.print.error('Timer already started')
         else:
             self.start_time = perf_counter()
             self.is_started = True
 
-    def pause(self):
+    def pause(self) -> None:
         if self.is_started:
             elapsed = perf_counter() - self.start_time
             self.time_data.append(elapsed)
@@ -31,7 +32,7 @@ class Timer:
         else:
             self.print.error('Timer already paused')
 
-    def resume(self):
+    def resume(self) -> None:
         if self.is_started:
             self.print.error('Timer already started')
         elif len(self.time_data) == 0:
@@ -46,7 +47,7 @@ class Timer:
 
         return format_time(total_time, self.ms_digits, self.format)
 
-    def stop(self):
+    def stop(self) -> None:
         if not self.is_started and len(self.time_data) == 0:
             self.print.error('Timer not started')
         else:
