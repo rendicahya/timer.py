a602d8a3d728a54ab88ffede68da9cd37e7f9994
rendicahya <rendicahya@gmail.com>
2021-02-10 05:51:31 +0700

    Printer now can use per method-call tag.

--- timerpy/printer.py
+++ timerpy/printer.py
@@ -5,8 +5,8 @@ class Printer:
     def __init__(self, tag: str):
         self.tag = tag
 
-    def error(self, text: str) -> None:
-        print_color(text, tag=self.tag, tag_color='red', color='white')
+    def error(self, text: str, tag: str = None) -> None:
+        print_color(text, tag=self.tag if tag is None else tag, tag_color='red', color='white')
 
-    def info(self, text: str) -> None:
-        print_color(text, tag=self.tag, tag_color='green', color='white')
+    def info(self, text: str, tag: str = None) -> None:
+        print_color(text, tag=self.tag if tag is None else tag, tag_color='green', color='white')
