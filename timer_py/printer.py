b89856993113b0e7c67604c1ac35b3e09f3ec9bb
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-03-28 12:52:24 +0900

    Removing the tag property from the class

--- timer_py/printer.py
+++ timer_py/printer.py
@@ -2,18 +2,12 @@ from print_color import print as print_color
 
 
 class Printer:
-    def __init__(self, tag: str):
-        self.tag = tag
-
-    def set_tag(self, tag: str) -> None:
-        self.__init__(tag)
-
     def error(self, text: str, tag: str = None) -> None:
         print_color(
-            text, tag=self.tag if tag is None else tag, tag_color="red", color="white"
+            text, tag=tag, tag_color="red", color="white"
         )
 
     def info(self, text: str, tag: str = None) -> None:
         print_color(
-            text, tag=self.tag if tag is None else tag, tag_color="green", color="white"
+            text, tag=tag, tag_color="green", color="white"
         )
