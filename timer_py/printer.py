f4d6b7d3c8fbbc252e801d9cf0a8ed902ceb6d95
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-03-28 13:02:14 +0900

    Setting background to black

--- timer_py/printer.py
+++ timer_py/printer.py
@@ -4,10 +4,10 @@ from print_color import print as print_color
 class Printer:
     def error(self, text: str, tag: str = None) -> None:
         print_color(
-            text, tag=tag, tag_color="red", color="white"
+            text, tag=tag, tag_color="red", color="white", background="black"
         )
 
     def info(self, text: str, tag: str = None) -> None:
         print_color(
-            text, tag=tag, tag_color="green", color="white"
+            text, tag=tag, tag_color="green", color="white", background="black"
         )
